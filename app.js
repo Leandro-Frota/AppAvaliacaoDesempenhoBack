e5c36c4eb0ab79662ea7651ffb19b04e86e3e163
Leandro <leandromfrota@gmail.com>
2024-10-09 22:17:23 -0300

    Added log e rateLimit middleware

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 import express from "express"
+import cors from "cors"
 import employeeRoutes from "./routes/employeeRoutes.js";
 import { connectDb } from './config/db.js';
+import logMiddleware from "./middleware/logMiddleware.js";
+import rateLimitMiddleware from "./middleware/rateLimiteMiddleware.js";
 
 const app = express()
 
 app.use(express.json());
+app.use(cors());
+
+app.use(logMiddleware);
+app.use(rateLimitMiddleware)
 
 const PORT = 3001
 
