0703066f1be947cebc0a08e12e7c3dfe7bef1b1c
leandro-frota <leandromfrota@gmail.com>
2025-06-03 22:26:05 -0300

    Fix: bug to connect DB

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 // app.use(logMiddleware);
 // app.use(rateLimitMiddleware)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
 app.use('/employees',employeeRoutes)
 
